a2dedfb4aec0253e3a1255a8fd93246318c59292
glasspinnA <ozze-1995@hotmail.com>
2018-12-19 21:08:39 +0100

    UI fixes

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -38,11 +38,12 @@ class ProfileScreen extends PureComponent {
 
     renderPageOne = () => {
         const deviceWidth = Dimensions.get('window').width
+
         return images.map((image, index) => {
             return(
                 <View key={index} style={[ {width: deviceWidth / 3}, {height: deviceWidth / 3} ]}>
-                    <Image style={{ flex: 1, width:undefined, height: undefined}} source={image}   />
-                                                             </View>
+                    <Image style={{ flex: 1, width:undefined, height: undefined}} source={image}/>
+                </View>
             )
         })
     }
@@ -70,10 +71,10 @@ class ProfileScreen extends PureComponent {
         <Content>
             <View>
                 <View style={{flexDirection: 'row'}}>
-                    <View style={{flex:1}}>
+                    <View style={{flex:1, marginVertical:10, marginHorizontal:10}}>
                         <Thumbnail source={{uri: 'https://facebook.github.io/react-native/img/showcase/facebook.png'}} />
                     </View>
-                    <View style={{flex: 3}}>
+                    <View style={{flex: 3, marginVertical:10, marginHorizontal:10}}>
                         <View style={{flexDirection:'row', justifyContent:'space-around'}}>
                             <View style={{alignItems:'center'}}>
                                 <Text>1</Text>
@@ -90,16 +91,16 @@ class ProfileScreen extends PureComponent {
                         </View>
 
                         <View style={{flexDirection: 'row',}}>
-                            <Button bordered dark style={{flex:2}}>
+                            <Button bordered dark style={{flex:2, justifyContent:'center', height:35, margin:5}}>
                                 <Text>Edit</Text>
                             </Button>
-                            <Button bordered dark style={{flex:1}}>
+                            <Button bordered dark style={{flex:1, justifyContent:'center', height:35, margin:5}}>
                                 <Text>Settings</Text>
                             </Button>
                         </View>
                     </View>
                 </View>
-                <View style={{paddingVertical:10, paddingHorizontal: 10}}>
+                <View style={{paddingVertical:5, paddingHorizontal: 20}}>
                     <Text>Test</Text>
                     <Text>Test</Text>
                     <Text>Test</Text>
@@ -118,7 +119,9 @@ class ProfileScreen extends PureComponent {
                         <Icon name="bookmark"style={[this.state.activeIndex == 3 ? {} : {color:'gray'}]}/>
                     </Button>
                 </View>
+                
                 {this.renderSection()}
+
             </View>
         </Content>
     </Container>
