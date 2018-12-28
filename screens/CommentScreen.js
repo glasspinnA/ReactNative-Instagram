101ea2704900f055da540bb64723076e98aa0e66
glasspinnA <ozze-1995@hotmail.com>
2018-12-28 16:22:38 +0100

    Comment Section UI Fixes

--- screens/CommentScreen.js
+++ screens/CommentScreen.js
@@ -1,49 +1,49 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import {Button, Item, Input, Content, Container, Left, Right,Body, Thumbnail, List, ListItem, Image } from 'native-base'
+import { Text, View, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import {Button, Item, Input, Content, Container, Left, Right,Body, Thumbnail, List, ListItem, } from 'native-base'
 import Fire from './../src/Fire'
 
 class CommentScreen extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-        commentText: '',
-        currentUserId:'',
-        currentUserUsername:'',
-        currentUserImageUrl:'',
-        postId: '',
-        post:{},
-        statusArray: [],
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            commentText: '',
+            currentUserId:'',
+            currentUserUsername:'',
+            currentUserImageUrl:'',
+            postId: '',
+            post:{},
+            statusArray: [],
+        };
+    }
+
+  
+    static navigationOptions = ({ navigation }) => {
+        return {
+          title: 'Comments',
+        };
+      };
+      
 
     componentDidMount(){ 
-
         const { navigation } = this.props;
         const postObject = navigation.getParam('postObject', 'NO-POST-OBJECT')
         
-        const postId = navigation.getParam('postId', 'NO-POST-ID');
-        const userUsername = navigation.getParam('userUsername', 'NO-POST-ID');
-        const userProfileImageUrl = navigation.getParam('userProfileImageUrl', 'NO-POST-ID');
-
         this.setState({
             post: postObject
         })
 
         this.fetchComments(postObject.postId)
-        
     }
 
     fetchComments = async (postId) =>{
         let responseArray = await Fire.shared.fetchComments(postId)
         
-
-      this.setState({
-        //Reverse so newst post is on top
-        statusArray: responseArray.reverse()
-      })
-    
+        this.setState({
+            //Reverse so newst post is on top
+            statusArray: responseArray.reverse()
+        })
     }
 
 
@@ -58,51 +58,76 @@ class CommentScreen extends Component {
         return this.state.statusArray.map((value,index) => {
           return(
                 <ListItem key={index} avatar>
-
-
-                <Left>
-                    <Thumbnail source={{uri:value.profileImageUrl}} />
-                </Left>
-                <Body>
-    
-                </Body>
-                
-                <Right>
-                    
-                <Text>{value.username}</Text>
-                    <Text note>{value.commentText}</Text>
-                    <Text note>{value.timestamp}</Text>
-                </Right>
+                    <Left>
+                        <Thumbnail source={{uri:value.profileImageUrl}} />
+                    </Left>
+                    <Body>
+                        <Text>{value.username}</Text>
+                        <Text note>{value.commentText}</Text>
+                    </Body>
+                    <Right>
+                        <Text note>{value.timestamp}</Text>
+                    </Right>
                 </ListItem>
-          )        
+            )        
         })
       }
 
+      /*
+
+
+      
+            
+      */
+
   render() {
     return (
-        <Container>
-            <Content>
+        <View style={styles.container}>
+            
+            <View style={styles.childrenBox}>
                 <List>
                     {this.renderComments()}
-                </List>
-            </Content>
-            <Content>
-                <View style={{flexDirection: 'row',}}>
-                    <Item style={{flex:3, justifyContent:'center', height:35, margin:5}}>
-                        <Input 
-                            placeholder="Enter your comment" 
-                            onChangeText={(commentInput) => 
-                                this.setState({commentText: commentInput})
-                            } />
-                    </Item>
-                    <Button onPress={this.postComment} style={{flex:1, justifyContent:'center', height:35, margin:5}}>
-                        <Text>Post Comment</Text>
+                </List>            
+            </View>
+            
+            <View style={styles.childrenBox}>
+                <View style={{flexDirection:'row'}}>
+                    <Body>
+                        <Item rounded>
+                            <Input 
+                                placeholder="Enter your comment" 
+                                onChangeText={(commentInput) => 
+                                    this.setState({commentText: commentInput})
+                                }
+                            />   
+                        </Item>
+                    </Body>
+                            
+                    <Button info rounded onPress={this.postComment} style={{marginLeft:10, padding:20}}>
+                        <Text>Post</Text>
                     </Button>
                 </View>
-            </Content>
-        </Container>
+            </View>
+            
+        </View>
     );
   }
 }
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        justifyContent: 'space-between',
+        alignContent: 'center',
+        alignItems: 'center',
+    },
+    childrenBox:{
+        padding:10,
+        width:'100%',
+    }
+})
+
 
 export default CommentScreen
